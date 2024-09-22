5f55b77db778f97888714e2dbf1ea6ba32f15654
Artem_Efimov <artsefimov@gmail.com>
2024-09-22 21:39:25 +0300

    profile

--- shop/shop_project/users/views.py
+++ shop/shop_project/users/views.py
@@ -1,10 +1,55 @@
 from django.shortcuts import render
+from .forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from django.contrib import auth
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username,
+                                     password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+
+    else:
+        form = UserLoginForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'users/login.html', context=context)
 
 
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'users/registration.html', context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {
+        'title': 'Store - Профиль',
+        'form': form
+    }
+    return render(request, 'users/profile.html', context)
