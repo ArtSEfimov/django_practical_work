b4654b953378fb7a48796efe088ac44f4fa85d49
Artem_Efimov <artsefimov@gmail.com>
2024-10-01 22:01:38 +0300

    new

---
+++ testogram/testogram/general/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    friends = models.ManyToManyField(
+        to="self",
+        symmetrical=True,
+        blank=True,
+    )
