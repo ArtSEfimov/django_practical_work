555a855d91b2db5f11c8936287043c02b5a34c91
Ефимов Артём Сергеевич <as.efimov@elteza.local>
2024-10-02 16:44:18 +0300

    admin

--- testogram/testogram/general/admin.py
+++ testogram/testogram/general/admin.py
@@ -1,3 +1,95 @@
 from django.contrib import admin
+from general.models import User, Post, Reaction, Comment
+from django.contrib.auth.models import Group
 
-# Register your models here.
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "username",
+        "email",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+        "date_joined",
+    )
+
+    fields = (
+        "first_name",
+        "last_name",
+        "username",
+        "password",
+        "email",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+        "friends",
+        "date_joined",
+        "last_login",
+    )
+
+    readonly_fields = (
+        "date_joined",
+        "last_login",
+    )
+
+
+@admin.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "author",
+        "title",
+        "get_body",
+        "created_at",
+        'get_comment_count',
+    )
+
+    def get_body(self, obj):
+        max_length = 64
+        if len(obj.body) > max_length:
+            return obj.body[:61] + "..."
+        return obj.body
+
+    get_body.short_description = "body"
+
+    def get_comment_count(self, obj):
+        return obj.comments.count()
+
+    get_comment_count.short_description = 'comments'
+
+    list_display_links = (
+        "id",
+        "get_body",
+        "title",
+    )
+
+
+@admin.register(Reaction)
+class ReactionModelAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "author",
+        "post",
+        "value",
+    )
+
+
+@admin.register(Comment)
+class CommentModelAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "author",
+        "post",
+        "body",
+        "created_at",
+    )
+    list_display_links = (
+        "id",
+        "body",
+    )
