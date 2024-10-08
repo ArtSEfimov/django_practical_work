926fb6eff1442e14f1b91b134a9dc2bd10a2598c
Artem <artsefimov@gmail.com>
2024-10-08 23:17:35 +0300

    blog_auth

--- blog_cbv/blog_cbv/apps/accounts/views.py
+++ blog_cbv/blog_cbv/apps/accounts/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.db import transaction
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, UpdateView
+from django.views.generic import DetailView, UpdateView, CreateView
 
-from apps.accounts.forms import ProfileUpdateForm, UserUpdateForm
+from apps.accounts.forms import ProfileUpdateForm, UserUpdateForm, UserRegisterForm, UserLoginForm
 from apps.accounts.models import Profile
 
 
@@ -19,7 +21,7 @@ class ProfileDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = f'Профиль пользователя: {self.object.user.username}'
-        return
+        return context
 
 
 class ProfileUpdateView(UpdateView):
@@ -56,3 +58,39 @@ class ProfileUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('profile_detail', kwargs={'slug': self.object.slug})
+
+
+class UserRegisterView(SuccessMessageMixin, CreateView):
+    """
+    Представление регистрации на сайте с формой регистрации
+    """
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('home')
+    template_name = 'accounts/user_register.html'
+    success_message = 'Вы успешно зарегистрировались. Можете войти на сайт!'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Регистрация на сайте'
+        return context
+
+
+class UserLoginView(SuccessMessageMixin, LoginView):
+    """
+    Авторизация на сайте
+    """
+    form_class = UserLoginForm
+    template_name = 'accounts/user_login.html'
+    next_page = 'home'
+    success_message = 'Добро пожаловать на сайт!'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Авторизация на сайте'
+        return context
+
+class UserLogoutView(LogoutView):
+    """
+    Выход с сайта
+    """
+    next_page = 'home'
