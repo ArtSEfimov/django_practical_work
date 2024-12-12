5d3d37802fab8d675d8308c835cae1d158b58454
Artem <artsefimov@gmail.com>
2024-12-12 22:06:45 +0300

    new_app

--- drf_education/crud/book/urls.py
+++ drf_education/crud/book/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
 from book import views
+from book.views import BookListView
+
+router = routers.DefaultRouter()
+router.register('api', BookListView)
 
 urlpatterns = [
     # path('', views.books_list, name='books_list'),
     # path('<int:pk>', views.book_detail, name='books_detail'),
-    path('', views.BookListView.as_view(), name='books_list'),
-    path('<int:pk>', views.BookDetailView.as_view(), name='books_detail')
+    #     path('', views.BookListView.as_view(), name='books_list'),
+    #     path('<int:pk>', views.BookDetailView.as_view(), name='books_detail')
+    path('', include(router.urls))
 ]
--- drf_education/crud/book/views.py
+++ drf_education/crud/book/views.py
@@ -1,52 +1,61 @@
 from django.http import Http404
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 from book.models import Book
 from book.serializers import BookSerializer
 
 
-# Create your views here.
-GenericAPIView
+# class BookListView(APIView):
+#     def get(self, request):
+#         books = Book.objects.all()
+#         serializer_books = BookSerializer(books, many=True)
+#         return Response(serializer_books.data)
+#
+#     def post(self, request):
+#         serializer_book = BookSerializer(data=request.data)
+#         if serializer_book.is_valid():
+#             serializer_book.save()
+#             return Response(serializer_book.data, status=201)
+#         return Response(serializer_book.errors, status=400)
+#
+#
+# class BookDetailView(APIView):
+#     def get_object(self, pk):
+#         try:
+#             return Book.objects.get(pk=pk)
+#         except Book.DoesNotExist:
+#             raise Http404
+#
+#     def get(self, request, pk):
+#         book = self.get_object(pk)
+#         serializer_book = BookSerializer(book)
+#         return Response(serializer_book.data)
+#
+#     def put(self, request, pk):
+#         book = self.get_object(pk)
+#         serializer_book = BookSerializer(book, data=request.data)
+#         if serializer_book.is_valid():
+#             serializer_book.save()
+#             return Response(serializer_book.data)
+#         return Response(serializer_book.errors, status=400)
+#
+#     def delete(self, request, pk):
+#         book = self.get_object(pk)
+#         book.delete()
+#         return Response(status=204)
 
-class BookListView(APIView):
-    def get(self, request):
-        books = Book.objects.all()
-        serializer_books = BookSerializer(books, many=True)
-        return Response(serializer_books.data)
 
-    def post(self, request):
-        serializer_book = BookSerializer(data=request.data)
-        if serializer_book.is_valid():
-            serializer_book.save()
-            return Response(serializer_book.data, status=201)
-        return Response(serializer_book.errors, status=400)
+class BookListView(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'pk'
 
-
-class BookDetailView(APIView):
-    def get_object(self, pk):
-        try:
-            return Book.objects.get(pk=pk)
-        except Book.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        book = self.get_object(pk)
-        serializer_book = BookSerializer(book)
-        return Response(serializer_book.data)
-
-    def put(self, request, pk):
-        book = self.get_object(pk)
-        serializer_book = BookSerializer(book, data=request.data)
-        if serializer_book.is_valid():
-            serializer_book.save()
-            return Response(serializer_book.data)
-        return Response(serializer_book.errors, status=400)
-
-    def delete(self, request, pk):
-        book = self.get_object(pk)
-        book.delete()
-        return Response(status=204)
+    @action(detail=True, methods=['get'])
+    def authors(self, request, pk=None):
+        book = self.get_object()
+        return Response({'author': book.author})
